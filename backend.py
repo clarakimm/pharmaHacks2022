a045a9332cf9f9204c36de93ed8cfbaeb1a46d3c
clara <ck3kim@uwaterloo.ca>
2022-03-27 08:41:38 -0400

    backend push

---
+++ backend.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+#from sklearn.externals import joblib
+
+mm_data = pd.read_csv("MM-DataSet_Complied_UnhealthyAndUnhealthy.csv", sep = ",")
+X = mm_data.drop(columns = ["CLASS"])
+y = mm_data['CLASS']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2)
+
+model = DecisionTreeClassifier()
+model = model.fit(X_train,y_train)
+
+userData = model.predict(X_test)
+userData
+
+score = accuracy_score(y_test, userData)
+score
